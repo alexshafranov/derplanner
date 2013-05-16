47064b2211ad73e96eedca2a99919eac465cb7c8
Alexander Shafranov <shafranov@gmail.com>
2013-05-16 20:59:48 +0400

    Missing.

---
+++ source/compiler/ast.cpp
@@ -0,0 +1,54 @@
+//
+// Copyright (c) 2013 Alexander Shafranov shafranov@gmail.com
+//
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software
+// in a product, an acknowledgment in the product documentation would be
+// appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+//
+
+#include <string.h>
+#include "derplanner/compiler/derplanner_assert.h"
+#include "derplanner/compiler/derplanner_memory.h"
+#include "derplanner/compiler/ast.h"
+
+namespace plnnrc {
+namespace ast {
+
+tree::tree()
+    : _memory(0)
+{
+    memset(&_root, 0, sizeof(_root));
+    _root.type = node_domain;
+}
+
+tree::~tree()
+{
+}
+
+node* tree::make_node(node_type type)
+{
+    return 0;
+}
+
+void tree::append_child(node* parent, node* child)
+{
+}
+
+void tree::remove_child(node* parent, node* child)
+{
+}
+
+}
+}
