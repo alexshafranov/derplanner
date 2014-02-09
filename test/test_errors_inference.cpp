b4fb4440d1fa487d94e409ebef2930a28c28babd
Alexander Shafranov <shafranov@gmail.com>
2014-02-09 14:27:26 +0100

    compiler: type inference error reporting tests.

---
+++ test/test_errors_inference.cpp
@@ -0,0 +1,32 @@
+//
+// Copyright (c) 2013 Alexander Shafranov shafranov@gmail.com
+//
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software
+// in a product, an acknowledgment in the product documentation would be
+// appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+//
+
+#include <unittestpp.h>
+#include <derplanner/compiler/errors.h>
+#include "test_errors.h"
+
+using namespace plnnrc;
+using namespace test;
+
+namespace
+{
+    TEST(_0) { check_error("(:domain (d) (:method (m x) (and\n(a x)) ()))", error_undefined, 2, 2); }
+    TEST(_1) { check_error("(:worldstate (w) (a (t))) (:domain (d) (:method (m x) (a\n(f x)) ()))", error_undefined, 2, 2); }
+}
