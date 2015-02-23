4fe2cd622ee641fd6f94ce6f6d978dc3d7653c70
Alexander Shafranov <shafranov@gmail.com>
2015-02-23 23:26:39 +0200

    sketch of a new runtime.

--- include/derplanner/runtime/types.h
+++ include/derplanner/runtime/types.h
@@ -25,7 +25,7 @@
 
 namespace plnnr {
 
-// Type IDs.
+// Type IDs supported by fact database.
 enum Type
 {
     Type_None = 0,
@@ -46,14 +46,14 @@ struct Fact_Type
     Type param_type[Max_Fact_Arity];
 };
 
-// Weak pointer to a fact tuple in Fact_Database
+// Weak pointer to a fact tuple in Fact_Database.
 struct Fact_Handle
 {
-    // index of table in Fact_Database
+    // index of table in Fact_Database.
     uint64_t table      : 20;
-    // index of entry in Fact_Table
+    // index of entry in Fact_Table.
     uint64_t entry      : 20;
-    // entry generation when this handle was obtained
+    // entry generation when this handle was obtained.
     uint64_t generation : 24;
 };
 
@@ -64,7 +64,7 @@ struct Fact_Table
     uint32_t        num_entries;
     // total allocated number of entries.
     uint32_t        max_entries;
-    // linear block of memory accommodating entries' data
+    // linear block of memory accommodating entries' data.
     void*           blob;
     // tuples laid out in SOA order.
     void*           columns[Max_Fact_Arity];
@@ -91,12 +91,37 @@ struct Fact_Database
     void*           blob;
 };
 
-//
-struct Planner_State;
-struct Method_Instance;
+// Header of a single frame on expansion stack. The frame layout is: Header | Arguments | Precondition_State.
+struct Expansion_Frame
+{
+    // in progress/expanded/failed flags.
+    uint32_t                flags       : 2;
+    // composite task type this frame holds data for, can be used for lookups in `Task_Info`.
+    uint32_t                task_type   : 15;
+    // index of the expanding case.
+    uint32_t                case_index  : 15;
+    // size of the parent (previous) frame.
+    uint32_t                parent_size;
+    // pointer to the expand function of that task/case.
+    Composite_Task_Expand   task_expand;
+};
+
+// Header of a single frame on task (plan) stack. The frame layout is: Header | Arguments.
+struct Task_Frame
+{
+    // composite/primitive task type.
+    uint32_t task_type;
+};
 
 //
-typedef bool (*Composite_Task_Expand)(Planner_State*, Method_Instance*, Fact_Database*);
+struct Planner_State
+{
+    Expansion_Frame*    top_expansion;
+    Task_Frame*         top_task;
+};
+
+//
+typedef bool (*Composite_Task_Expand)(Planner_State*, Expansion_Frame*, Fact_Database*);
 
 // Database construction parameters.
 struct Database_Format
@@ -124,7 +149,9 @@ struct Task_Info
     uint32_t*               hashes;
     // task names (composite tasks are specified after primitive).
     const char**            names;
-    // pointer to generated "expand" function for each composite task.
+    // format of task parameters.
+    Fact_Type*              parameters;
+    // pointer to generated expand function for each composite task.
     Composite_Task_Expand*  expands;
 };
 
@@ -133,6 +160,9 @@ class Domain
 {
 public:
     virtual ~Domain() {}
+
+    // initialize domain information.
+    virtual void init() = 0;
 
     // format of the fact database required by this domain.
     virtual Database_Format get_database_requirements() const = 0;
