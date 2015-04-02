bb9f9a1d5ea5ebf99f5e01c62efa224d517b35a8
Alexander Shafranov <shafranov@gmail.com>
2015-04-02 22:10:14 +0300

    compiler: parser ast node allocation helpers.

--- source/compiler/parser.cpp
+++ source/compiler/parser.cpp
@@ -20,6 +20,7 @@
 
 #include <string.h>
 #include "derplanner/compiler/assert.h"
+#include "derplanner/compiler/memory.h"
 #include "derplanner/compiler/parser.h"
 #include "derplanner/compiler/lexer.h"
 #include "derplanner/compiler/id_table.h"
@@ -47,6 +48,58 @@ void plnnrc::destroy(Parser& state)
     destroy(state.task_ids);
     destroy(state.fact_ids);
     memset(&state, 0, sizeof(Parser));
+}
+
+template <typename T>
+T* allocate(Parser& state)
+{
+    return static_cast<T*>(allocate(state.pool, sizeof(T), plnnrc_alignof(T)));
+}
+
+template <typename T>
+T* allocate(Parser& state, size_t count)
+{
+    return static_cast<T*>(allocate(state.pool, sizeof(T)*count, plnnrc_alignof(T)));
+}
+
+static ast::Fact_Type* alloc_Fact_Type(Parser& state, uint32_t num_params)
+{
+    ast::Fact_Type* result = allocate<ast::Fact_Type>(state);
+    result->num_params = num_params;
+    result->param_types = allocate<Token_Type>(state, num_params);
+    return result;
+}
+
+static ast::World* alloc_World(Parser& state, uint32_t num_facts)
+{
+    ast::World* result = allocate<ast::World>(state);
+    result->num_facts = num_facts;
+    result->facts = allocate<ast::Fact_Type*>(state, num_facts);
+    return result;
+}
+
+static ast::Task* alloc_Task(Parser& state, uint32_t num_cases, uint32_t num_params)
+{
+    ast::Task* result = allocate<ast::Task>(state);
+    result->num_cases = num_cases;
+    result->num_params = num_params;
+    result->param_names = allocate<Token_Value>(state, num_params);
+    result->param_types = allocate<Token_Type>(state, num_params);
+    result->cases = allocate<ast::Case*>(state, num_cases);
+    return result;
+}
+
+static ast::Case* alloc_Case(Parser& state, uint32_t num_tasks)
+{
+    ast::Case* result = allocate<ast::Case>(state);
+    result->num_tasks = num_tasks;
+    result->task_list = allocate<ast::Expr*>(state, num_tasks);
+    return result;
+}
+
+static ast::Expr* alloc_Expr(Parser& state)
+{
+    return allocate<ast::Expr>(state);
 }
 
 void plnnrc::parse(Parser& /*state*/)
