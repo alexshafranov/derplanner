e25f6fe7eea7677f49396bd28e17e799e813b1f0
Alexander Shafranov <shafranov@gmail.com>
2015-05-04 23:20:09 +0300

    runtime: moved max plan length check.

--- source/runtime/planning.cpp
+++ source/runtime/planning.cpp
@@ -135,18 +135,18 @@ Find_Plan_Status plnnr::find_plan_step(Fact_Database* db, Planning_State* state)
             {
                 return Find_Plan_Succeeded;
             }
-
-            // check if maximum plan length reached.
-            if (size(state->task_stack) == state->max_plan_length)
-            {
-                return Find_Plan_Max_Plan_Length_Reached;
-            }
         }
 
         // check if maximum expansion depth reached.
         if (size(state->expansion_stack) == state->max_depth)
         {
             return Find_Plan_Max_Depth_Reached;
+        }
+
+        // check if maximum plan length reached.
+        if (size(state->task_stack) == state->max_plan_length)
+        {
+            return Find_Plan_Max_Plan_Length_Reached;
         }
     }
     else
