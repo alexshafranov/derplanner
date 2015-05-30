e1c88181fea44c77f66a3ac7605258b903797427
Alexander Shafranov <shafranov@gmail.com>
2015-05-30 20:49:33 +0300

    compiler: added script to run compiler tests, added travis build step.

---
+++ test/run_cases.py
@@ -0,0 +1,56 @@
+# Copyright (c) 2015 Alexander Shafranov shafranov@gmail.com
+# 
+# This software is provided 'as-is', without any express or implied
+# warranty.  In no event will the authors be held liable for any damages
+# arising from the use of this software.
+#
+# Permission is granted to anyone to use this software for any purpose,
+# including commercial applications, and to alter it and redistribute it
+# freely, subject to the following restrictions:
+#
+# 1. The origin of this software must not be misrepresented; you must not
+# claim that you wrote the original software. If you use this software
+# in a product, an acknowledgment in the product documentation would be
+# appreciated but is not required.
+# 2. Altered source versions must be plainly marked as such, and must not be
+# misrepresented as being the original software.
+# 3. This notice may not be removed or altered from any source distribution.
+
+import os
+import glob
+import types
+import unittest
+import functools
+import subprocess
+
+class Main(unittest.TestCase):
+    pass
+
+def read_expected(domain_path):
+    result = []
+    for line in open(domain_path, 'rb'):
+        if line.startswith('//>'):
+            result += [line.lstrip('//>').strip()]
+    return result
+
+def make_test(domain_path):
+
+    def test(self):
+        expected = read_expected(domain_path)
+        exe = os.path.join('bin', 'x64', 'debug', 'derplannerc')
+        p = subprocess.Popen(args=[exe, '-o', '.', domain_path], stderr=subprocess.PIPE)
+        (stdoutdata, stderrdata) = p.communicate()
+        actual = [line.strip() for line in stderrdata.splitlines()]
+        self.assertEqual(expected, actual)
+
+    return test
+
+def generate(base_path):
+    domains = glob.glob(os.path.join(base_path, '*.domain'))
+    for domain_path in domains:
+        setattr(Main, 'test#' + domain_path, make_test(domain_path))
+
+
+if __name__ == '__main__':
+    generate(base_path=os.path.join(os.path.dirname(__file__), 'cases'))
+    unittest.main()
