f7e4c00065581366baf4a62bf9bf1c94b19faa28
Alexander Shafranov <shafranov@gmail.com>
2015-06-03 23:57:35 +0300

    compiler: fixed planning success check in runtime tests.

--- test/test_run_0.cpp
+++ test/test_run_0.cpp
@@ -92,7 +92,7 @@ plnnr::add_entry(b, 2);
 
 
     plnnr::Find_Plan_Status status = plnnr::find_plan(domain, &db, &pstate);
-    CHECK_EQUAL(status, plnnr::Find_Plan_Succeeded);
+    CHECK_EQUAL(plnnr::Find_Plan_Succeeded, status);
 
     check_plan("t!(1, 2)", pstate, domain);
 
